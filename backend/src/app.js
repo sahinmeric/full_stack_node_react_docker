3590a12b3083da3bc64c7a5e5c1197eefdfaa440
Sahin Meric <sahin.meric@medisprout.com>
2024-08-23 18:35:53 -0500

    database connected

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,9 @@ const sequelize = require('./config/db');
 
 dotenv.config();
 
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+
 const app = express();
 
 app.use(express.json());
--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,3 +1,7 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
