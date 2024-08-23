7182056c458e57b9d77c2059a672deacd5cca747
Sahin Meric <sahin.meric@medisprout.com>
2024-08-23 16:22:12 -0500

    server configuration

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
